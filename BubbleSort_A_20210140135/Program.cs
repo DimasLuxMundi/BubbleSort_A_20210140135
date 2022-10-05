599c507683fbf1f18de0911af21b188074f03a55
DimasLuxMundi <dimasluxmundi@gmail.com>
2022-10-05 17:23:25 +0700

    Deklarasi array dan variabel

--- BubbleSort_A_20210140135/Program.cs
+++ BubbleSort_A_20210140135/Program.cs
@@ -6,8 +6,14 @@ using System.Threading.Tasks;
 
 namespace BubbleSort_A_20210140135
 {
-    internal class Program
+    class Program
     {
+        //Deklarasi array int dengan ukuran 20
+        private int[] a = new int[20];
+
+        //Deklarasi variabel int untuk menyimpan banyaknya data pada array
+        private int[] n;
+
         static void Main(string[] args)
         {
         }
